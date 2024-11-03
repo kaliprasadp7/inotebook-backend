95047a56f7b6131977500c61fb7c44e5b4ed3e5d
KALI PRASAD PANDA <kaliprasaadpanda@KALIs-MacBook-Air.local>
2024-11-03 22:57:26 +0530

    inttaled bcryptjs and added salt and hash to store secure password in db

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 // Create a user using: POST "/api/auth/createuser" doesn't require auth
 router.post('/createuser', [
@@ -14,16 +15,19 @@ router.post('/createuser', [
     if (!result.isEmpty()) {
         return res.status(400).json({ errors: result.array() });
     }
-    // Check wheather the user exist already or not
     try {
+        // Check wheather the user exist already or not
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "Sorry the user is already exists" });
         }
+
+        const salt = await bcrypt.genSaltSync(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
         res.send(user);
     }
