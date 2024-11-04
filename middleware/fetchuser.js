d3a41d8ea8dbe84f2279f655ac563003a891fa90
KALI PRASAD PANDA <kaliprasaadpanda@KALIs-MacBook-Air.local>
2024-11-04 14:19:04 +0530

    middle ware and fetuser added to get user data using auth-token

---
+++ middleware/fetchuser.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "mysecretsignature%#@";
+
+
+const fetchuser = (req, res, next) => {
+// Get the user from the jwt token and add it to the req object
+const token = req.header('auth-token');
+if(!token){
+    res.status(401).send({error : "Please authenticate using a valid token"})
+}
+try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.user = data.user;
+    next();
+} catch (error) {
+    res.status(401).send({error : "Please authenticate using a valid token",
+        message: error.message
+    })
+}
+}
+
+module.exports = fetchuser;
--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,11 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = "mysecretsignature%#@"
+const fetchuser = require('../middleware/fetchuser');
+const JWT_SECRET = "mysecretsignature%#@";
 
-// Create a user using: POST "/api/auth/createuser" doesn't require auth
+
+// ROUTE:1 Create a user using: POST "/api/auth/createuser" doesn't require auth
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid Email').isEmail(),
@@ -37,7 +39,7 @@ router.post('/createuser', [
                 id:user.id
             }
         }
-        const authtoken = jwt.sign(data, 'JWT_SECRET');
+        const authtoken = jwt.sign(data, JWT_SECRET);
         res.send({authtoken});
     }
     catch (error) {
@@ -51,7 +53,7 @@ router.post('/createuser', [
 
 
 
-// Login a user using: POST "/api/auth/login" No login required
+// ROUTE:2 Login a user using: POST "/api/auth/login" No login required
 router.post('/login', [
     body('email', 'Enter a valid Email').isEmail(),
     body('password', 'Password cannot be empty').exists()
@@ -79,12 +81,25 @@ router.post('/login', [
                 id:user.id
             }
         }
-        const authtoken = jwt.sign(data, 'JWT_SECRET');
+        const authtoken = jwt.sign(data, JWT_SECRET);
         res.send({authtoken});
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Internal server error");
     }
 })
+
+// ROUTE:3 Get user data using: POST "/api/auth/getuser" Login required
+router.post('/getuser', fetchuser, async (req, res) => {
+try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);  
+} catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+}
+})
+
 
 module.exports = router
