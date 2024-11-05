c3026e3bad510b9f4bd4ddc21dc4bb5ba352639a
KALI PRASAD PANDA <kaliprasaadpanda@KALIs-MacBook-Air.local>
2024-11-05 10:25:42 +0530

    try catch added for update and delete functions

--- routes/notes.js
+++ routes/notes.js
@@ -12,9 +12,9 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
     res.json(notes)
 
   } catch (error) {
-        console.log(error.message);
-        res.status(500).send("Internal server error");
-    }
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
 })
 
 // ROUTE:2 Add a new note using: POST "/api/notes/addnote" Login required
@@ -24,59 +24,68 @@ router.post('/addnote', fetchuser, [
 ], async (req, res) => {
 
   try {
-    const {title, description, tag} = req.body;
-  // If there are errors return bad request and the errors
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
+    const { title, description, tag } = req.body;
+    // If there are errors return bad request and the errors
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
       return res.status(400).json({ errors: result.array() });
-  }
+    }
 
-  const note = new Note({title, description, tag, user:req.user.id})
-  const savedNote = await note.save();
+    const note = new Note({ title, description, tag, user: req.user.id })
+    const savedNote = await note.save();
 
-  res.json(savedNote);
+    res.json(savedNote);
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Internal server error");
-}
-
+  }
 })
 
 // ROUTE:3 Update note using: PUT "/api/notes/updatenote" Login required
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
 
-  const {title, description, tag} = req.body;
-  //create newNote object
-  const newNote = {};
-  if(title){newNote.title = title}
-  if(description){newNote.description = description}
-  if(tag){newNote.tag = tag}
+  try {
+    const { title, description, tag } = req.body;
+    //create newNote object
+    const newNote = {};
+    if (title) { newNote.title = title }
+    if (description) { newNote.description = description }
+    if (tag) { newNote.tag = tag }
 
-  //Find the note to be updated and update it
-  let note = await Note.findById(req.params.id);
-  if(!note){return res.status(404).send("Not found")}
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) { return res.status(404).send("Not found") }
 
-  if(note.user.toString() !==  req.user.id){
-    return res.status(404).send("Not found");
+    if (note.user.toString() !== req.user.id) {
+      return res.status(404).send("Not found");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json({ note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
   }
-
-  note = await Note.findByIdAndUpdate(req.params.id, {$set:newNote}, {new:true})
-  res.json({note});
-
 })
+
 
 // ROUTE:4 Delete note using: DELETE "/api/notes/deletenote" Login required
 router.delete('/deletenote/:id', fetchuser, async (req, res) => {
-  //Find the note to be updated and update it
-  let note = await Note.findById(req.params.id);
-  if(!note){return res.status(404).send("Not found")}
+  try {
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) { return res.status(404).send("Not found") }
 
-  if(note.user.toString() !==  req.user.id){
-    return res.status(404).send("Not found");
+    if (note.user.toString() !== req.user.id) {
+      return res.status(404).send("Not found");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.send({ "Success": "Note has been deleted succesfully", note: note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
   }
-
-  note = await Note.findByIdAndDelete(req.params.id);
-  res.send({"Success" : "Note has been deleted succesfully", note:note});
 })
 
 module.exports = router
